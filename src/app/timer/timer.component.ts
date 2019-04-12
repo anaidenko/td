1d8746ab5e5851f3e3d6b3e5ce4060aa5eda42cf
anaidenko <Andrii.Naidenko@outlook.com>
2019-04-12 18:15:38 +0200

    Add toggler for timer component

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timer.component.scss']
 })
 export class TimerComponent implements OnInit {
+  state: 'running' | 'paused' = 'paused';
+  action: 'play' | 'pause' = 'play';
+
   constructor() {}
 
   ngOnInit() {}
+
+  toggle() {
+    if (this.state === 'running') {
+      this.state = 'paused';
+      this.action = 'play';
+    } else {
+      this.state = 'running';
+      this.action = 'pause';
+    }
+  }
 }
