0b86a0b4f1865ef285cc34cbb1314a48604d1756
anaidenko <Andrii.Naidenko@outlook.com>
2019-04-12 19:37:43 +0200

    Connect timer to todo model

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, timer, BehaviorSubject, combineLatest, of } from 'rxjs';
-import { map, startWith, switchMap } from 'rxjs/operators';
+import { Component, OnInit, Input } from '@angular/core';
+import { Observable, timer, BehaviorSubject, of, combineLatest } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs/operators';
+import { Todo } from '../todo';
+
+// const DEFAULT_TIMER_SEC = 10; // 10 sec
+const DEFAULT_TIMER_SEC = 30 * 60; // 30 min
 
 @Component({
   selector: 'app-timer',
@@ -11,29 +15,73 @@ export class TimerComponent implements OnInit {
   state: 'running' | 'paused' = 'paused';
   action: 'play' | 'pause' = 'play';
 
-  playing$ = new BehaviorSubject<boolean>(false);
+  private _todo: Todo;
+
+  @Input()
+  set todo(todo: Todo) {
+    this._todo = todo;
+    this.setupTimer(todo);
+  }
+
+  private playingSubj = new BehaviorSubject<boolean>(false);
+  private timeSpentSubj = new BehaviorSubject<number>(0);
+  private timeRemainingSubj = new BehaviorSubject<number>(0);
+
+  interval$: Observable<number>;
   timer$: Observable<number>;
-  progress$: Observable<number>;
+  timeSpent$: Observable<number>;
+  timeRemaining$: Observable<number>;
+  timeRemainingProgress$: Observable<number>;
 
   constructor() {
-    this.timer$ = timer(1000, 1000).pipe(startWith(0));
-    this.progress$ = this.playing$.pipe(
-      switchMap(playing => (playing ? this.timer$ : of(0))),
-      map(i => Math.max(0, 100 - i))
-    );
+    this.initObservables();
   }
 
   ngOnInit() {}
 
+  initObservables() {
+    this.interval$ = timer(0, 1000);
+
+    this.timer$ = this.playingSubj.pipe(switchMap(playing => (playing ? this.interval$ : of(0))));
+
+    this.timeSpent$ = combineLatest(this.timeSpentSubj, this.timer$).pipe(
+      map(([timeSpent, timer]) => (timeSpent || 0) + (timer || 0)),
+      tap(value => (this._todo.timeSpent = value))
+    );
+
+    this.timeRemaining$ = combineLatest(this.timeRemainingSubj, this.timer$).pipe(
+      map(([timeRemaining, timer]) => (typeof timeRemaining === 'number' ? timeRemaining : DEFAULT_TIMER_SEC) - (timer || 0)),
+      tap(value => (this._todo.timeRemaining = value))
+    );
+
+    this.timeRemainingProgress$ = combineLatest(this.timeSpent$, this.timeRemaining$).pipe(
+      map(([timeSpent, timeRemaining]) => (timeSpent + timeRemaining ? (timeRemaining / (timeSpent + timeRemaining)) * 100 : 0))
+    );
+  }
+
+  setupTimer(todo: Todo) {
+    this.timeSpentSubj.next(todo ? todo.timeSpent : 0);
+    this.timeRemainingSubj.next(todo ? todo.timeRemaining : 0);
+  }
+
   toggle() {
     if (this.state === 'running') {
-      this.state = 'paused';
-      this.action = 'play';
-      this.playing$.next(false);
+      this.pause();
     } else {
-      this.state = 'running';
-      this.action = 'pause';
-      this.playing$.next(true);
+      this.play();
     }
+  }
+
+  play() {
+    this.state = 'running';
+    this.action = 'pause';
+    this.playingSubj.next(true);
+  }
+
+  pause() {
+    this.state = 'paused';
+    this.action = 'play';
+    this.playingSubj.next(false);
+    this.setupTimer(this._todo);
   }
 }
--- src/app/todo.ts
+++ src/app/todo.ts
@@ -2,6 +2,8 @@ export class Todo {
   id: number;
   title = '';
   complete = false;
+  timeSpent: number;
+  timeRemaining: number;
 
   constructor(values: Object = {}) {
     Object.assign(this, values);
